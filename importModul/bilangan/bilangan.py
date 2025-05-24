9654c2882f474fdb98a8b42d7e961d83638364c2
denindraa <mhmmddenindra12@gmail.com>
2025-05-24 11:20:30 +0700

    update ngab

---
+++ importModul/bilangan/bilangan.py
@@ -0,0 +1,8 @@
+def ke_biner(n):
+    return bin(n)
+
+def ke_oktal(n):
+    return oct(n)
+
+def ke_heksadesimal(n):
+    return hex(n)
---
+++ salah/ubahBilangan/bilangan.py
@@ -0,0 +1,8 @@
+def ke_biner(nilai):
+    return bin(nilai)[2:]
+
+def ke_oktal(nilai):
+    return oct(nilai)[2:]
+
+def ke_heksadesimal(nilai):
+    return hex(nilai)[2:]
